0b3a30ce6088add6101c1643fd395dbd4e22edd2
Dan Morse <djmthedev@gmaill.com>
2023-03-04 15:48:55 -0800

    add experiment

---
+++ Azam/Azam/AzamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzamApp.swift
+//  Azam
+//
+//  Created by Morse on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AzamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
